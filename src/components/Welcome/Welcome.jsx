6a26ad616e509866f2f61d0fd8714cc56e658359
Furkan Özdemir <ozthefur@gmail.com>
2023-09-24 02:13:39 +0300

    info text added

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,11 +1,13 @@
 import styles from "./Welcome.module.scss";
 import Navbar from "../Navbar/Navbar";
 import TrustedBy from "../TrustedBy/TrustedBy";
+import InfoText from "../InfoText/InfoText";
 
 const Welcome = () => {
   return (
     <div className={styles.welcome}>
       <Navbar />
+      <InfoText />
       <TrustedBy />
     </div>
   );
