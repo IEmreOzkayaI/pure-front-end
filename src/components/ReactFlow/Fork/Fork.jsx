698159dac24ca0c0f83a42352cc4de0ccc3841d6
Furkan Özdemir <ozthefur@gmail.com>
2023-12-23 17:36:06 +0300

    fork made resizable

--- src/components/ReactFlow/Fork/Fork.jsx
+++ src/components/ReactFlow/Fork/Fork.jsx
@@ -1,32 +1,60 @@
-import { Handle, Position } from "reactflow";
+import {
+  Handle,
+  Position,
+  NodeResizer,
+  useUpdateNodeInternals,
+} from "reactflow";
 import styles from "./Fork.module.scss";
+import { useCallback } from "react";
 
-function Fork({ data, isConnectable }) {
-  console.log("data", data);
+function Fork({ data, isConnectable, id }) {
+  const updateNodeInternals = useUpdateNodeInternals();
+  const changeBottomHandlePosition = useCallback(() => {
+    const bottomHandle = document.querySelector(
+      `[data-handleid="forkSourceDown_${id}"]`
+    );
+
+    const bottomResizer = document.querySelector(`#fork_${id}`);
+
+    bottomHandle.style.top = `${
+      bottomResizer.previousElementSibling.offsetTop - 16
+    }px`;
+
+    updateNodeInternals(id);
+  }, [updateNodeInternals, id]);
   return (
-    <div className={styles.fork}>
-      <Handle
-        type="target"
-        position={Position.Left}
-        id="forkTarget"
-        className={styles.handleLeft}
-        isConnectable={isConnectable}
+    <>
+      <NodeResizer
+        isVisible
+        minHeight={70}
+        minWidth={3}
+        maxWidth={3}
+        onResize={changeBottomHandlePosition}
       />
-      <Handle
-        type="target"
-        position={Position.Right}
-        id="forkSourceUp"
-        className={styles.handleUp}
-        isConnectable={isConnectable}
-      />
-      <Handle
-        type="source"
-        position={Position.Right}
-        id="forkSourceDown"
-        className={styles.handleDown}
-        isConnectable={isConnectable}
-      />
-    </div>
+      <div className={styles.fork} id={`fork_${id}`}>
+        <Handle
+          type="target"
+          position={Position.Left}
+          id="forkTarget"
+          className={styles.handleLeft}
+          isConnectable={isConnectable}
+        />
+        <Handle
+          type="target"
+          position={Position.Right}
+          id="forkSourceUp"
+          className={styles.handleUp}
+          isConnectable={isConnectable}
+        />
+        <Handle
+          type="source"
+          position={Position.Right}
+          id={`forkSourceDown_${id}`}
+          className={styles.handleDown}
+          isConnectable={isConnectable}
+        />
+      </div>
+    </>
   );
 }
 
