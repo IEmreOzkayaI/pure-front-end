4b04aac4979009e07bec0b8d9bf6a7f7078badfd
Furkan Özdemir <ozthefur@gmail.com>
2023-12-23 17:35:56 +0300

    decisionNode made resizable

--- src/components/ReactFlow/DecisionNode/DecisionNode.jsx
+++ src/components/ReactFlow/DecisionNode/DecisionNode.jsx
@@ -1,23 +1,27 @@
-import { Handle, Position } from "reactflow";
+import { Handle, Position, NodeResizer } from "reactflow";
 import styles from "./DecisionNode.module.scss";
 
 function DecisionNode({ data, isConnectable }) {
   console.log("data", data);
   return (
-    <div className={styles.decision}>
-      <Handle
-        type="target"
-        position={Position.Left}
-        id="decisionNodeTarget"
-        isConnectable={isConnectable}
-      />
-      <Handle
-        type="source"
-        position={Position.Right}
-        id="decisionNodeSource"
-        isConnectable={isConnectable}
-      />
-    </div>
+    <>
+      <NodeResizer isVisible minWidth={50} minHeight={50} />
+
+      <div className={styles.decision}>
+        <Handle
+          type="target"
+          position={Position.Left}
+          id="decisionNodeTarget"
+          isConnectable={isConnectable}
+        />
+        <Handle
+          type="source"
+          position={Position.Right}
+          id="decisionNodeSource"
+          isConnectable={isConnectable}
+        />
+      </div>
+    </>
   );
 }
 
